9016ceebcbd2feb663beee4b9e05cf4f2cbb6386
yohan fairfort <yohan.fairfort@gmail.com>
2019-04-17 17:16:51 +0200

    switch from WCF to Nancy

---
+++ HomeModule.cs
@@ -0,0 +1,16 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get("/", _ => "Hello World!");
+        }
+    }
+}
