47741313bf49af4b53390dd8866e0f1db04489bd
yohan fairfort <yohan.fairfort@gmail.com>
2019-06-27 18:54:19 +0200

    Add Napster Secret

---
+++ Napster/SpotifySecrets.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api
+{
+    public class NapsterSecrets
+    {
+        public string NapsterId { get; set; }
+        public string NapsterSecret { get; set; }
+
+        public NapsterSecrets() { }
+    }
+}
