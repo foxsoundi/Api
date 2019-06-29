0c2a5dd6ea50cd075f6fde812754be2b3a7e1249
yohan fairfort <yohan.fairfort@gmail.com>
2019-06-29 21:03:47 +0200

    Add of Signup route

--- Modules/PlayerModule.cs
+++ Modules/PlayerModule.cs
@@ -21,6 +21,12 @@ namespace Api.Modules
                 CredentialDto credentialDto = this.Bind<CredentialDto>();
                 return Response.AsJson(playerConnection.Login(credentialDto));
             });
+
+            Post("signUp", parameters =>
+            {
+                SignUpDto signUpDto = this.Bind<SignUpDto>();
+                return Response.AsJson(playerConnection.SignUp(signUpDto));
+            });
             Get("info/{token}", parameters =>
             {
                 Guid sessionToken = Guid.Parse(parameters.token);
